6348fd8c8373ea6d17094aa31b4ecd0c7b2f0b77
GauthamSrini <gauthamsrinivasan21@gmail.com>
2025-04-12 14:57:39 +0530

    deleted some files

---
+++ src/controllers/sugar_levels.js
@@ -0,0 +1,18 @@
+const { get_query_database,post_query_database } = require("../config/database_utils")
+
+const get_sugar_levels = async(req,res)=>{
+    try{
+        const {user_id} = req.query;
+        const query = `SELECT week_1,week_2,week_3,week_4 FROM sugar_levels WHERE user_id = ?`;
+        const data = await get_query_database(query,[user_id])
+        res.status(200).json(data)
+    }
+    catch(err){
+        console.log("error while fetching sugar levels :",err);
+        res.send(500).json({
+            err : "error fetching sugar levels"
+        })
+    }
+}
+
+module.exports = {get_sugar_levels}
--- src/routes/glyco_care_routes.js
+++ src/routes/glyco_care_routes.js
@@ -2,6 +2,8 @@ const express = require("express")
 const dietary_choices = require("../controllers/dietary_choices")
 const medical_prescription = require("../controllers/medical_prescription")
 const validate_user = require("../controllers/validte_user")
+const sugar_levels = require("../controllers/sugar_levels")
+const heart_rates = require("../controllers/heart_rates")
 
 
 const router = express.Router()
@@ -11,5 +13,8 @@ router.post("/prescription/create",medical_prescription.create_prescription)
 router.post("/prescription/view",medical_prescription.get_pdf)
 router.get("/prescription/getAll",medical_prescription.get_prescriptions)
 router.get("/validateUser",validate_user.validate_user)
+router.get("/getSugarLevels",sugar_levels.get_sugar_levels);
+router.get("/getHeartRates",heart_rates.get_heart_rates);
+
 
 module.exports = router;
